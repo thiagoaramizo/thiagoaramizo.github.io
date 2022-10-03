0faffce44b4872f328039900e2015a3fa70c9977
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2022-10-03 19:09:46 -0300

    Integração com a API de perfil da Alura

--- src/components/Cursos/Curso/Curso.tsx
+++ src/components/Cursos/Curso/Curso.tsx
@@ -2,18 +2,31 @@ import cursos from '../../../data/cursos.json'
 import styles from './Curso.module.scss'
 import { BiBuildings, BiCalendarCheck, BiTimeFive, BiCertification } from "react-icons/bi";
 
-type props = typeof cursos[0] 
+interface cursoType {
+    author: string 
+    code: string
+    color: string
+    finishedCourses: number 
+    finishedSteps: number
+    id: number 
+    kind: string 
+    lastAccessTime: number 
+    name: string 
+    totalCourses: number
+    totalSteps: number
+    url: string
+}
 
-export default function Curso( {...curso}: props) {
+
+export default function Curso( {...curso}: cursoType) {
 
     return (
         <div className={styles.curso}>
-            <h3>{curso.nome}</h3>
+            <h3>{curso.name}</h3>
             <div className={styles.curso__info}>
-                <span><BiCalendarCheck className={styles.curso__info__icone} /> {curso.data}</span>
-                <span><BiBuildings className={styles.curso__info__icone}/> {curso.escola}</span>
-                <span><BiTimeFive className={styles.curso__info__icone}/> {curso.cargaHoraria}h</span>
-                { curso.certificado != '' ? <span><a target={'_blank'} href={curso.certificado}><BiCertification className={styles.curso__info__icone}/> Certificado</a></span> : ''}
+                <span><BiCalendarCheck className={styles.curso__info__icone} /> {curso.lastAccessTime}</span>
+                <span><BiBuildings className={styles.curso__info__icone}/> Alura</span>
+                <span><BiTimeFive className={styles.curso__info__icone}/> {curso.finishedCourses}/{curso.totalCourses}</span>
             </div>
         </div>
     )
--- src/components/Cursos/Cursos.tsx
+++ src/components/Cursos/Cursos.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react'
 import cursos from '../../data/cursos.json'
 import Curso from './Curso/Curso'
 import styles from './Cursos.module.scss'
 
+interface cursoType {
+    author: string 
+    code: string
+    color: string
+    finishedCourses: number 
+    finishedSteps: number
+    id: number 
+    kind: string 
+    lastAccessTime: number 
+    name: string 
+    totalCourses: number
+    totalSteps: number
+    url: string
+}
+
 export default function Cursos() {
+
+    const [cursos, setCursos] = useState<cursoType[]>([])
+
+    useEffect( () => {
+        const aluraApi = 'https://www.alura.com.br/api/dashboard/c1c6e10478775682c8f700d6456de4a33aa2149722db71baeff41021b4902ed5'
+        fetch(aluraApi).then(response => {
+            response.json().then(respostaJson => {
+                setCursos( respostaJson.guides )
+            } )
+        })
+    }, [])
 
     return (
         <section className={styles.cursos}>
